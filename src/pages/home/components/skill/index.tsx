3714ae19b0bf1189d17aed4eb3fd1df1bd239835
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-07 18:18:11 +0100

    fixed excess padding issue on small screens

--- src/pages/home/components/skill/index.tsx
+++ src/pages/home/components/skill/index.tsx
@@ -7,7 +7,7 @@ import OtherSkills from "./OtherSkills.component";
 
 export default function SkillSection() {
   return (
-    <StyledSection className=" bg-deep-blue-900 pt-[9rem] pb-[21rem] ">
+    <StyledSection className=" bg-deep-blue-900 pt-[4rem] sm:pt-[9rem] pb-[18rem] sm:pb-[21rem] ">
       <div className=" flex flex-col gap-x-16 gap-y-16 xl:flex-row ">
         <AboutMe />
         <div id="skills" className=" flex flex-col md:flex-row gap-x-9 gap-y-9 font-bold text-sm text-white w-full ">
