aa8da0d7239e77ae5969b8dd72209efd7669c6fe
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-08 12:57:59 +0100

    resized padding and reduced contact link size for small screen

--- src/components/footer/FooterLink.component.tsx
+++ src/components/footer/FooterLink.component.tsx
@@ -4,7 +4,7 @@ export interface FooterLinkProps extends React.PropsWithChildren {}
 
 export default function FooterLink(props: FooterLinkProps) {
   return (
-    <button className=" h-12 aspect-square rounded-lg bg-deep-blue-700 grid place-items-center ">
+    <button className=" h-6 md:h-12 aspect-square rounded-lg bg-deep-blue-700 grid place-items-center ">
       {props.children}
     </button>
   );
