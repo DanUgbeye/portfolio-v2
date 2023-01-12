797874539632796f3cfc48b944fbf111b7786f62
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-12 19:00:53 +0100

    feat: added header to services section

--- src/pages/home/components/services/index.tsx
+++ src/pages/home/components/services/index.tsx
@@ -6,12 +6,16 @@ import Service from "./Service.component";
 export default function ServicesSection() {
   return (
     <StyledSection className=" -mt-[14rem] ">
-      <div id="services" className=" grid grid-cols-[repeat(auto-fit,_minmax(15rem,_1fr))] gap-x-5 gap-y-5 ">
+      <div className=" w-full ">
+        <h4 className=" font-bold mb-3 text-center ">MY SERVICES</h4>
+      </div>
+
+      <div
+        id="services"
+        className=" grid grid-cols-[repeat(auto-fit,_minmax(15rem,_1fr))] gap-x-5 gap-y-5 "
+      >
         {services.map((service, index) => (
-          <Service
-            key={index}
-            {...service}
-          />
+          <Service key={index} {...service} />
         ))}
       </div>
     </StyledSection>
