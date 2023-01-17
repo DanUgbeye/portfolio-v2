0fb2162171afeadcf74b1c0bf5da371a77b40589
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-17 16:12:26 +0100

    fix: corrected wrong import statements

--- src/data/skills/backendSkills.data.ts
+++ src/data/skills/backendSkills.data.ts
@@ -2,7 +2,7 @@ import NodeJsIcon from "../../assets/svg/skills/backend/nodejs.svg";
 import ExpressJsIcon from "../../assets/svg/skills/backend/expressjs.svg";
 import PhpIcon from "../../assets/svg/skills/backend/php.svg";
 import MySqlIcon from "../../assets/svg/skills/backend/mysql.svg";
-import PostgreSqlIcon from "../../assets/svg/skills/backend/postgreSql.svg";
+import PostgreSqlIcon from "../../assets/svg/skills/backend/postgresql.svg";
 import MongoDbIcon from "../../assets/svg/skills/backend/mongodb.svg";
 import PostmanIcon from "../../assets/svg/skills/backend/postman.svg";
 import SwaggerIcon from "../../assets/svg/skills/backend/swagger.svg";
