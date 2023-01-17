5995ec4eb0b9052a8c8119ee3f2b00cbf494f5fa
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-17 16:15:56 +0100

    fix: corrected more wrong import statements

--- src/data/skills/otherSkills.data.tsx
+++ src/data/skills/otherSkills.data.tsx
@@ -2,7 +2,7 @@ import CppIcon from "../../assets/svg/skills/other/c++.svg";
 import ArduinoIcon from "../../assets/svg/skills/other/arduino.svg";
 import GitIcon from "../../assets/svg/skills/other/git.svg";
 import GithubIcon from "../../assets/svg/skills/other/github.svg";
-import VsCodeIcon from "../../assets/svg/skills/other/vsCode.svg";
+import VsCodeIcon from "../../assets/svg/skills/other/vscode.svg";
 
 const otherSkills = [
   {
