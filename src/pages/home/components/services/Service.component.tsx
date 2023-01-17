064062c367b4a64b3f694c388cecb1f04fab48a8
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-17 12:14:26 +0100

    feat: added scale animation on hover for service card

--- src/pages/home/components/services/Service.component.tsx
+++ src/pages/home/components/services/Service.component.tsx
@@ -12,7 +12,7 @@ export interface ServiceProps {
 
 export default function Service(props: ServiceProps) {
   return (
-    <div className=" bg-deep-blue-700 hover:bg-violet-light px-6 md:px-9 py-8 md:py-12 rounded-lg md:rounded-none shadow-round-lg shadow-black/20 flex flex-col gap-y-6 transition-colors duration-200 w-full max-w-sm mx-auto group ">
+    <div className=" bg-deep-blue-700 hover:bg-violet-light px-6 md:px-9 py-8 md:py-12 rounded-lg md:rounded-none shadow-round-lg shadow-black/20 flex flex-col gap-y-6 transition-all duration-200 w-full max-w-sm mx-auto group hover:scale-105 ">
       <props.svg
         height={64}
         width={64}
