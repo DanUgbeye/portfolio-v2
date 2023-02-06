3cd654191c6949334e35fb9b8237ef748049015d
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-02-06 18:25:50 +0100

    fix: resolved padding issue with project and testimonial section

--- src/pages/home/components/testimonials/index.tsx
+++ src/pages/home/components/testimonials/index.tsx
@@ -3,13 +3,13 @@ import StyledSection from "../../../../components/StyledSection.component";
 import testimonials from "../../../../data/testimonials/testimonials.data";
 import Testimonial from "./Testimonial.component";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function TestimonialSection() {
   return (
     <StyledSection className=" bg-deep-blue-900 ">
-      <div className=" pt-[18rem] sm:pt-[20rem] pb-[4rem] sm:pb-[8.5rem] relative ">
+      <div className=" pt-[10rem] sm:pt-[15rem] pb-[4rem] sm:pb-[8.5rem] relative ">
         <h4 className=" font-bold mb-3 ">TESTIMONIALS</h4>
 
         <p className=" text-sm font-normal max-w-lg mb-2 ">
