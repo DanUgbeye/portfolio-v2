58fbf08adebb6c367871d79acdc7c8de3d008bf9
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-02-09 12:05:33 +0100

    fix: changed styling to grid in SkillList component and reduced font size for Skill component

--- src/pages/home/components/about/SkillList.component.tsx
+++ src/pages/home/components/about/SkillList.component.tsx
@@ -27,13 +27,13 @@ export default function SkillList() {
   return (
     <div
       id="skills"
-      className=" flex flex-col gap-x-9 gap-y-9 font-bold text-sm text-white w-full "
+      className=" flex flex-col gap-x-9 gap-y-10 font-bold text-sm text-white w-full "
     >
       {allSkills.map((skillType: SkillType, index) => (
         <div key={index} className=" min-w-fit w-full ">
-          <h4 className=" mb-4 ">{skillType.name}</h4>
+          <h4 className=" mb-1 ">{skillType.name}</h4>
 
-          <div className=" flex flex-wrap justify-between md:justify-start gap-x-4 gap-y-4 ">
+          <div className=" grid grid-cols-[repeat(auto-fill,_minmax(9rem,_1fr))] md:justify-start gap-1 ">
             {skillType.skills.map((skill, index) => (
               <Skill key={index} {...skill} />
             ))}
