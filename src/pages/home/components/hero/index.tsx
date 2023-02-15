ec94a58cb2f130e533279ed833a523d45b84f34b
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-02-15 09:47:48 +0100

    feat: added animation to hero section

--- src/pages/home/components/hero/index.tsx
+++ src/pages/home/components/hero/index.tsx
@@ -2,16 +2,58 @@ import React from "react";
 import IntroText from "./IntroText.component";
 import HeroSvg from "../../../../assets/svg/hero.svg";
 import StyledSection from "../../../../components/StyledSection.component";
+import { motion, Variants } from "framer-motion";
+
+const HeroSectionVariants: Variants = {
+  inView: {
+    opacity: 1,
+    transition: {
+      delay: 0.1,
+      when: "beforeChildren"
+    },
+  },
+
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const HeroImageVariants: Variants = {
+  inView: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.2
+    },
+  },
+
+  hidden: {
+    opacity: 0,
+    x: 1000,
+  },
+};
 
 export default function HeroSection() {
   return (
     <StyledSection className="  ">
-      <div className=" flex flex-col md:flex-row gap-x-2 gap-y-4 items-center mt-10 ">
+      <motion.div
+        variants={HeroSectionVariants}
+        initial="hidden"
+        whileInView="inView"
+        className=" flex flex-col md:flex-row gap-x-2 gap-y-4 items-center mt-10 overflow-hidden "
+      >
         <IntroText />
-        <div className=" w-full ">
-          <img src={HeroSvg} className={" mx-auto max-w-[25rem] md:max-w-none w-full -translate-y-14 "} alt="hero" />
-        </div>
-      </div>
+        <motion.div variants={HeroImageVariants} className=" w-full ">
+          <img
+            loading="eager"
+            src={HeroSvg}
+            className={
+              " mx-auto max-w-[25rem] md:max-w-none w-full -translate-y-6 "
+            }
+            alt="hero"
+          />
+        </motion.div>
+      </motion.div>
     </StyledSection>
   );
 }
