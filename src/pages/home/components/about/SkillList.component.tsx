cccb8601a54abb35b12c00afce7aac18450eee16
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-02-17 14:35:24 +0100

    feat: added animation to about section

--- src/pages/home/components/about/SkillList.component.tsx
+++ src/pages/home/components/about/SkillList.component.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Skill from "./Skill.component";
 import { SkillType } from "../../../../../app.interface";
+import { Variants, motion } from "framer-motion";
 
 // data import
 import Skills from "../../../../data/skills/skills.data";
+
+const SkillListVariants: Variants = {
+  inView: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.2,
+    },
+  },
+
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+};
 
 export default function SkillList() {
   const allSkills = React.useMemo<SkillType[]>(
@@ -25,12 +41,22 @@ export default function SkillList() {
   );
 
   return (
-    <div
+    <motion.div
+      transition={{
+        staggerChildren: 0.4,
+        when: "beforeChildren"
+      }}
       id="skills"
       className=" flex flex-col gap-x-9 gap-y-10 font-bold text-sm text-white w-full "
     >
       {allSkills.map((skillType: SkillType, index) => (
-        <div key={index} className=" min-w-fit w-full ">
+        <motion.div
+          variants={SkillListVariants}
+          whileInView="inView"
+          initial="hidden"
+          key={Math.random()}
+          className=" min-w-fit w-full "
+        >
           <h4 className=" mb-1 ">{skillType.name}</h4>
 
           <div className=" grid grid-cols-[repeat(auto-fill,_minmax(9rem,_1fr))] md:justify-start gap-1 ">
@@ -38,8 +64,8 @@ export default function SkillList() {
               <Skill key={index} {...skill} />
             ))}
           </div>
-        </div>
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 }
