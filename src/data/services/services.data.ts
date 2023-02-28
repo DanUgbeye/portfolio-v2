a70241d66f0072c4902c2f3e4d39cee470755a21
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-02-28 16:17:49 +0100

    task: added service section copy

--- src/data/services/services.data.ts
+++ src/data/services/services.data.ts
@@ -3,30 +3,25 @@ import { ReactComponent as BackendIcon } from "../../assets/svg/back-end-icon.sv
 import { ReactComponent as FullStackIcon } from "../../assets/svg/full-stack-icon.svg";
 import { ReactComponent as IoTIcon } from "../../assets/svg/internet-of-things.svg";
 
-// TODO: work on the svg colors
 const services = [
   {
     name: "Frontend Web Development",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-      Vitae neque, quisque ac dictum. Ultricies vel vulputate elementum ultrices.`,
+    description: `Using the latest front-end technologies, I create beautiful, intuitive, dynamic and responsive websites that engage users and enhance their experience`,
     svg: FrontendIcon,
   },
   {
     name: "Backend Web Development",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-      Vitae neque, quisque ac dictum. Ultricies vel vulputate elementum ultrices.`,
+    description: `I have expertise in building robust and scalable back-end systems for web applications that can handle complex data processing, security, and application logic`,
     svg: BackendIcon,
   },
   {
     name: "Full Stack Web Development",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-      Vitae neque, quisque ac dictum. Ultricies vel vulputate elementum ultrices.`,
+    description: `With skills in both front-end and back-end development, I provide end-to-end web application development for businesses of all sizes that meet business needs`,
     svg: FullStackIcon,
   },
   {
     name: "IOT & Embedded Systems Prog.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-      Vitae neque, quisque ac dictum. Ultricies vel vulputate elementum ultrices.`,
+    description: `I specialize in creating software for IoT and embedded systems which connects devices and sensors, collects and processes data, and enables real-time decision making`,
     svg: IoTIcon,
   },
 ];
