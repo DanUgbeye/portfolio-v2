92b5137b0ea226253eb97d30de8150dfed73540a
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-02-28 16:15:45 +0100

    fix: centralised animation to service.variants file and made minor UI changes

--- src/pages/home/components/services/index.tsx
+++ src/pages/home/components/services/index.tsx
@@ -1,29 +1,18 @@
-import { motion, Variants } from "framer-motion";
 import React from "react";
+import { motion } from "framer-motion";
 import StyledSection from "../../../../components/StyledSection.component";
 import services from "../../../../data/services/services.data";
+import { randomKey } from "../../../../utils/randomKey.util";
 import Service from "./Service.component";
-
-const ServiceSectionVariants: Variants = {
-  inView: {
-    y: 0,
-    opacity: 1,
-    transition: { type: "tween", ease: "easeInOut" },
-  },
-
-  hidden: {
-    y: 100,
-    opacity: 0,
-  },
-};
+import { serviceHeadingSlideFromTop } from "./service.variants";
 
 export default function ServicesSection() {
   return (
     <StyledSection className=" -mt-[14rem] ">
       <div className=" w-full ">
         <motion.h4
-          variants={ServiceSectionVariants}
-          whileInView="inView"
+          variants={serviceHeadingSlideFromTop}
+          animate="visible"
           initial="hidden"
           className=" font-bold text-center mb-10 "
         >
@@ -34,8 +23,8 @@ export default function ServicesSection() {
           id="services"
           className=" grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,_minmax(15rem,_1fr))] gap-x-4 gap-y-10 "
         >
-          {services.map((service, index) => (
-            <Service key={index} {...service} />
+          {services.map((service) => (
+            <Service key={randomKey()} {...service} />
           ))}
         </div>
       </div>
